43378e24dfeb6845370de6a5364a9e899ca5bad6
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-09-11 23:15:02 +0200

    add redux-persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,6 @@ import { getFilter } from 'redux/filter/filter-selectors';
 
 import {Filter} from './Filter/Filter';
 
-// const initialContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-
 export function App() {
   const contacts = useSelector(getFilteredContacts);
   const filter = useSelector(getFilter);
